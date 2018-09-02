acd0c1b57926fe47b88e6fbe02c1dcb5ad994e89
Rafael Marcilio <rafaelbatistamarcilio@gmail.com>
2018-09-02 00:59:29 -0300

    change env port

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -6,7 +6,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port: any = process.env.PORT || 8080;
+  const port: any = process.env.PORT || 3000;
   app.setGlobalPrefix('api');
   app.use(cors());
   await app.listen(port, () => {
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port: any = process.env.PORT || 8080;
+  const port: any = process.env.PORT || 3000;
   app.setGlobalPrefix('api');
   app.use(cors());
   await app.listen(port, () => {
