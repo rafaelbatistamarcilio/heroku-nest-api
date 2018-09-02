5aaecfd4ec390f6382a005b7048af16d2ab92148
Rafael Marcilio <rafaelbatistamarcilio@gmail.com>
2018-09-02 00:47:08 -0300

    change node version to default

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port: any = process.env.PORT || 8080;
-	app.setGlobalPrefix('api');
-	app.use(cors());
-  await app.listen(port);
+  app.setGlobalPrefix('api');
+  app.use(cors());
+  await app.listen(port, () => {
+    console.log(`Our app is running on port ${port}`);
+  });
 }
 bootstrap();
