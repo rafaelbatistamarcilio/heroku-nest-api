63f1d4c896b94387857b6dd8922f6c36849d3881
Rafael Marcilio <rafaelbatistamarcilio@gmail.com>
2018-09-02 01:38:24 -0300

    node version

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -6,12 +6,10 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port: any = process.env.PORT || 3000;
-  app.setGlobalPrefix('api');
-  app.use(cors());
-  await app.listen(port, () => {
-    console.log(`Our app is running on port ${port}`);
-  });
+	const port: any = process.env.PORT || 3001;
+	app.setGlobalPrefix('api');
+	app.use(cors());
+	await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,9 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port: any = process.env.PORT || 3000;
-  app.setGlobalPrefix('api');
-  app.use(cors());
-  await app.listen(port, () => {
-    console.log(`Our app is running on port ${port}`);
-  });
+	const port: any = process.env.PORT || 3001;
+	app.setGlobalPrefix('api');
+	app.use(cors());
+	await app.listen(port);
 }
 bootstrap();
